1f371af66fb868132a28690bcd4ca1735dc97725
Tomás Mataloni <tomimatalonib@gmail.com>
2023-09-13 22:09:51 -0300

    Actualizado 2

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -6,7 +6,7 @@ const localFavs = JSON.parse(localStorage.getItem('favs'))
 const initialFavState = localFavs ? localFavs : []
 export const initialState = {theme: "", data: []}
 
-const ContextGlobal = createContext(undefined);
+const ContextGlobal = createContext();
 
 export const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
