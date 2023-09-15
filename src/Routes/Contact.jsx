a5a8a4f5225b6661dcd7c2360bb43a126bfebf10
Tomás Mataloni <tomimatalonib@gmail.com>
2023-09-15 20:40:37 -0300

    Actualizacion 3

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import Form from '../Components/Form'
+import { useNavigate } from 'react-router-dom'
+
 const Contact = () => {
 
     const navigate = useNavigate()
